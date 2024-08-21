db610882fc1507fe592486b6a7b0765e6062ca73
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-08-21 16:31:58 +0300

    student class

---
+++ src/main/java/com/example/EduTrack/EduTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduTrackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/EduTrack/Entity/Student.java
@@ -0,0 +1,42 @@
+package com.example.EduTrack.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double GPA;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(Double GPA) {
+        this.GPA = GPA;
+    }
+}
